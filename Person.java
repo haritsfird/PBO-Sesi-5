323b84cd0299f62d38f5c653bfe00fac2ff82c21
haritsfird <92732116+haritsfird@users.noreply.github.com>
2022-10-25 21:24:46 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,30 @@
+
+package encapsulation;
+
+public class Person {
+     private final String name;
+    private String address;
+   
+   public Person(String name, String address) {
+      this.name = name;
+      this.address = address;
+   }
+   
+   public String getName() {
+      return name;
+   }
+  
+   
+   public String getAddress() {
+      return address;
+   }
+   
+   public void setAddress(String address) {
+      this.address = address;
+   }
+   
+   @Override
+   public String toString() {
+      return name + "(" + address + ")";
+   }
+}
